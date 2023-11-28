4840ed0b9467527c551a8debed3dce0362aafdd5
Roberto Costa <roberto.araujo339@gmail.com>
2023-11-27 22:34:35 -0300

    Add MovieCard component and update homepage

---
+++ src/components/MovieCard.tsx
@@ -0,0 +1,119 @@
+import { FiHeart, FiShoppingCart } from 'react-icons/fi';
+
+type MovieCardProps = {
+  movie: {
+    id: number;
+    title: string;
+    genre: string;
+    cover: string;
+  };
+};
+
+const MovieCard = ({ movie }: MovieCardProps) => {
+  const handleAddToCart = () => {
+    alert('Adicionando ao carrinho...');
+  };
+
+  const handleAddToWishlist = () => {
+    alert('Adicionando à lista de desejos...');
+  };
+
+  return (
+    <div
+      className='
+      w-72
+      max-w-[300px]
+      h-auto
+      flex
+      flex-col
+      justify-start
+      items-center
+      bg-zinc-300
+      rounded-md
+      p-4
+      gap-4
+  '
+    >
+      <h2
+        className='
+        text-xl
+        font-bold
+        text-center
+        text-zinc-800
+      '
+      >
+        {movie.title}
+      </h2>
+
+      <img
+        src={movie.cover}
+        alt={movie.title}
+        className='
+        w-full
+        h-auto
+        rounded-md            
+    '
+      />
+
+      <div
+        className='
+      w-full
+      flex
+      flex-row
+      justify-between
+      items-center
+      gap-4
+      '
+      >
+        <button
+          className='
+        w-full
+        bg-zinc-800
+        text-zinc-400
+        font-bold
+        rounded-md
+        p-2
+        hover:bg-zinc-900
+        hover:text-zinc-500
+      '
+        >
+          Ver mais
+        </button>
+
+        <button
+          onClick={handleAddToCart}
+          className='
+            w-fit
+            bg-zinc-800
+            text-zinc-400
+            font-bold
+            rounded-md
+            p-2
+            hover:bg-zinc-900
+            hover:text-zinc-500
+      '
+        >
+          <FiShoppingCart size={24} />
+        </button>
+
+        <button
+          onClick={handleAddToWishlist}
+          className='
+            w-fit
+            bg-zinc-800
+            text-zinc-400
+            font-bold
+            rounded-md
+            p-2
+            hover:bg-zinc-900
+            hover:text-zinc-500
+          '
+        >
+          <FiHeart size={24} />
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default MovieCard;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,46 @@
 import Header from '../components/Header';
+import MovieCard from '../components/MovieCard';
+import movies from '../data/movies.json';
 
 const Home = () => {
   return (
     <>
       <Header />
-      <h1>hi</h1>
+
+      <main
+        className='
+        pt-20
+        w-full
+        min-h-screen
+        flex
+        flex-col
+        justify-start
+        items-center
+        bg-zinc-200
+        gap-4
+      '
+      >
+        <h1 className='text-3xl font-bold text-center'>
+          Os melhores filmes de todos os tempos, você encontra aqui!
+        </h1>
+
+        <div
+          className='
+            w-full
+            flex
+            flex-row
+            flex-wrap
+            justify-evenly
+            items-start
+            gap-4
+            p-8
+        '
+        >
+          {movies.map((movie) => (
+            <MovieCard key={movie.id} movie={movie} />
+          ))}
+        </div>
+      </main>
     </>
   );
 };
